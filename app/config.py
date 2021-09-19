ae42b29f63bd96ca916adce576829279a0c8bf76
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2021-09-19 17:59:31 -0500

    Produccion v0.1

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 class Config:
     SECRET_KEY = 'super_secret'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mauro:mauro9812@localhost/flaskmysql'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mauro:mauro9812@localhost/flaskmysql'
+    SLQALCHEMY_DATABASE_URI = 'postgres://ndndythzfvhrca:16f8ba0ff2386ad7d03df7d5866ada2b216ae336043b0b69abf5df6db40488bb@ec2-52-203-74-38.compute-1.amazonaws.com:5432/d3npmpir4tem4k'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ENV = 'development'
 
