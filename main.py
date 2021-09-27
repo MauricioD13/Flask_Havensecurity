a334f72e2f66e0ac7e7e4944dee59f49554d2508
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2021-09-26 21:49:02 -0500

    Fase 2 produccion v0.1.2

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ def index():
     """
     user_ip = request.remote_addr
     response = make_response(redirect('haven'))
-    session['user_ip'] = user_ip
     return response
 
 
@@ -124,5 +123,3 @@ def server_error(error):
     return render_template('Error_500.html', error=error)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
