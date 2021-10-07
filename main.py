d6b5f00c7b47417de83962e4b605a7d057aa3545
Mauricio Cuello <mdavid_cuello@javeriana.edu.co>
2021-10-07 15:20:44 -0500

    Fase 2.1 Produccion v0.4

--- main.py
+++ main.py
@@ -117,6 +117,3 @@ def not_found(error):
 @app.errorhandler(500)
 def server_error(error):
     return render_template('Error_500.html', error=error)
-
-if __name__ == '__main__':
-    app.run(debug=True)
